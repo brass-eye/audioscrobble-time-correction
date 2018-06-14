4b68018ddb131f23d83cbf3ea0c7a798437c39bd
Aiden <aidenscott12@googlemail.com>
2018-06-14 14:01:30 +0100

    Add read and write corrected file function

--- audioscrobble_time_offset.py
+++ audioscrobble_time_offset.py
@@ -1,6 +1,25 @@
+import sys
+import os
+
 def offset_audioscrobbler_line(scrobble, offset):
     scrobble_parts = scrobble.split("\t")
-    time = int(scrobble_parts[-1])
-    scrobble_parts[-1] = str(time + offset)
+    time = int(scrobble_parts[-2])
+    scrobble_parts[-2] = str(time + offset)
 
     return "\t".join(scrobble_parts)
+
+scrobble_file = open(sys.argv[1], 'r' )
+output_file = open(sys.argv[1] + "_corrected", 'w' )
+offset = int(sys.argv[2])
+
+for scrobble in scrobble_file:
+    if scrobble[0] != "#":
+        output_file.write(offset_audioscrobbler_line(scrobble, offset))
+    else:
+        output_file.write(scrobble)
+
+output_file.close()
+scrobble_file.close()
+
+
+
--- test.py
+++ test.py
@@ -1,15 +1,22 @@
 import unittest
 from audioscrobble_time_offset import *
+from io import StringIO
 
 class AudioScrobbleTimeOffsetTests(unittest.TestCase):
 
     def test_offset_audioscrobbler_line(self):
         old_time = 946705463
-        old_scrobble = "Boris\tAmplifier Worship\tGanbou-Ki\t2\t969\tL\t946705463"
+        old_scrobble = "Boris\tAmplifier Worship\tGanbou-Ki\t2\t969\tL\t946705463\n"
         ten_hours = 60*60*10
         expected_time = old_time + ten_hours
         expected_scrobble = "Boris\tAmplifier Worship\tGanbou-Ki\t2\t969\tL\t{}".format(expected_time)
         self.assertEqual(offset_audioscrobbler_line(old_scrobble, ten_hours), expected_scrobble)
+    
+    # def ignores_comments(self):
+    #     comment = "#hello"
+    #     self.assertEqual(comment, offset_audioscrobbler_line(comment, 0))
+
+
 
 if __name__ == '__main__':
     unittest.main()
